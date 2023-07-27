b3261f5662eb9a7244ec0cb44d10ddeae1caa457
RimpiSingh <rimpis4825@gmail.com>
2023-07-27 16:33:07 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def data_render(request):
+    d={'name':'rimpi','age':15}
+    return render(request,'data_render.html',context=d)
