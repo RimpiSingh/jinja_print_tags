3f77da6cebc15764c1b2cad7c9e1e79035dd20e6
RimpiSingh <rimpis4825@gmail.com>
2023-08-18 09:08:48 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -3,5 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def data_render(request):
-    d={'name':'rimpi','age':15}
+    d={'name':'kajal','age':15}
     return render(request,'data_render.html',context=d)
+
+def condition(request):
+    d={'a':30,'b':100,'c':80,'hobbies':['cricket','football']}
+    return render(request,'condition.html',context=d)
+
